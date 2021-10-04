f56da2bc196304aea6906ce8997d396e1165fca3
sorted-Tech0 <shivanesforcode@gmail.com>
2021-10-04 18:49:49 +0530

    error boundary component in React

---
+++ 10-error-boundaries/src/Results.js
@@ -0,0 +1,27 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>No pets Found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            animal={pet.animal}
+            key={pet.id}
+            name={pet.name}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city} ${pet.state}`}
+            id={pet.id}
+          />
+
+          //   <Pet {...pet} key={pet.id} />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
